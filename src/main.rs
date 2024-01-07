5e22c8ebcf157890e5a3ac0a7648dd003626cb22
Noa Moulin <noa.moulin42@gmail.com>
2024-01-07 21:00:02 +0100

    ca avance

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,88 @@
 use image::{Rgb, RgbImage};
+use std::fs;
 
 fn main() {
+    let path: &str = "/home/noa/Desktop/igctest/";
 
-    let path = "/home/noa/Desktop/igctest"
+    let names = list_files(path);
 
-    let names: Vec<&str> = vec![
-    "20-50-1956.jpg",  "22-54-0000-R.jpg",  "24-53-1986.jpg",  "26-47-1981.jpg",
-    "20-51-1964.jpg",  "23-47-1955.jpg",    "24-54-1972.jpg",  "26-48-1990.jpg",
-    "20-52-1971.jpg",  "23-48-1979.jpg",    "24-55-1978.jpg",  "26-49-1979.jpg",
-    "21-49-1989.jpg",  "23-49-1975.jpg",    "25-46-1993.jpg",  "26-50-1992.jpg",
-    "21-50-1988.jpg",  "23-50-1932.jpg",    "25-47-1986.jpg",  "26-51-0000-R1.jpg",
-    "21-51-1989.jpg",  "23-52-1964.jpg",    "25-48-1990.jpg",  "26-52-0000-R.jpg",
-    "21-52-1971.jpg",  "23-53-1970.jpg",    "25-49-1980.jpg",  "26-53-1982-R.jpg",
-    "21-53-1971.jpg",  "23-54-1974.jpg",    "25-50-1985.jpg",  "26-54-1979.jpg",
-    "22-48-1996.jpg",  "24-46.jpg",         "25-51-1968.jpg",  "27-47-1955.jpg",
-    "22-49-1979.jpg",  "24-48-1978.jpg",    "25-52-1967.jpg",  "27-48-1984.jpg",
-    "22-50-1995.jpg",  "24-49-1987.jpg",    "25-53.jpg",       "27-49-1966.jpg",
-    "22-51-1995.jpg",  "24-50-1988.jpg",    "25-54-1978.jpg",  "27-50-1971.jpg",
-    "22-52-1964.jpg",  "24-51-1965.jpg",    "25-55-1966.jpg",  "27-51-1966.jpg",
-    "22-53-1970.jpg",  "24-52-1968.jpg",    "26-46-1897.jpg",  "27-52-1989.jpg"];
-    
+    let images: Vec<String> = names
+        .into_iter()
+        .filter(|elm| elm.ends_with(".jpg"))
+        .collect();
+    let elues: Vec<(String, Vec<u32>)> = images
+        .into_iter()
+        .map(|name| {
+            (
+                name.clone(),
+                name.split('-')
+                    .filter(|elm| elm.chars().all(|chr| chr.is_numeric()))
+                    .map(|n| n.parse::<u32>().unwrap())
+                    .collect(),
+            )
+        })
+        .collect();
+    let planches: Vec<(String, Vec<u32>)> =
+        elues.into_iter().filter(|elm| elm.1.len() == 2).collect();
+    let positions_x: Vec<u32> = planches.iter().map(|elm| elm.1[0]).collect();
+    let positions_y: Vec<u32> = planches.iter().map(|elm| elm.1[1]).collect();
 
-    let images: Vec<&str> = names.into_iter().filter(|elm| elm.ends_with(".jpg")).collect();
-    let elues: Vec<(&str, Vec<i32>)> = images.into_iter().map(|name| (name, name.split('-').filter(|elm| elm.chars().all(|chr| chr.is_numeric())).map(|n| n.parse::<i32>().unwrap()).collect())).collect();
-    let planches: Vec<(&str, Vec<i32>)> = elues.into_iter().filter(|elm| elm.1.len() == 2).collect();
-    let positions_x: Vec<i32> = planches.iter().map(|elm| elm.1[0]).collect();
-    let positions_y: Vec<i32> = planches.iter().map(|elm| elm.1[1]).collect();
-    
-    println!("Px : {:?} | Py : {:?}", positions_x, positions_y);
+    let xlims = (
+        positions_x.iter().min().unwrap(),
+        positions_x.iter().max().unwrap(),
+    );
+    let ylims = (
+        positions_y.iter().min().unwrap(),
+        positions_y.iter().max().unwrap(),
+    );
 
-    let xlims = (positions_x.iter().min().unwrap(), positions_x.iter().max().unwrap());
-    let ylims = (positions_y.iter().min().unwrap(), positions_y.iter().max().unwrap());
+    let planches_floor: Vec<(String, u32, u32, (_, _))> = planches
+        .into_iter()
+        .map(|elm| {
+            (
+                path.to_string() + elm.0.clone().as_str(),
+                elm.1[0] - xlims.0,
+                elm.1[1] - ylims.0,
+                image::image_dimensions(path.to_string() + elm.0.clone().as_str())
+                    .expect("Aucune image dans le repertoire selectionné"),
+            )
+        })
+        .collect();
+
+    for elm in &planches_floor {
+        println!("{:?}\n", elm);
+    }
+
+    let tile_size = (planches_floor.clone().into_iter().map(|elm| elm.3.0).max().unwrap(), planches_floor.clone().into_iter().map(|elm| elm.3.1).max().unwrap());
     let nwidth = xlims.1 - xlims.0;
     let nheight = ylims.1 - ylims.0;
 
-    let planches_floor: Vec<(&str, i32, i32, (_, _))>= planches.into_iter().map(|elm| (path + elm.0, elm.1[0] - xlims.0, elm.1[1] - ylims.0, image::image_dimensions(elm.0).expect("Aucune image dans le repertoire selectionné"))).collect();
+    println!("tile size : {:?}", tile_size);
 
-    println!("Xlims : {:?} | Ylims : {:?}", xlims, ylims);
-    println!("{nwidth} images en largeur | {nheight} images en hauteur");
-    println!("A assembler : {:?}", planches_floor);
+    let render_size: (u32, u32) = (tile_size.0 * nwidth, tile_size.1 * nheight);
+
+    println!("render size : {:?}", render_size);
+
+/////////////////////////////////////////////////////////////
+    let mut image_buffer = RgbImage::new(100, 100);
+
+    for (_, _, pixel) in image_buffer.enumerate_pixels_mut() {
+        *pixel = Rgb([255, 255, 255]);
+    }
+
+    if let Err(e) = image_buffer.save("image.png") {
+        eprintln!("Erreur lors de l'enregistrement de l'image : {}", e);
+    }
+//////////////////////////////////////////////////////////////
+}
+
+fn list_files(chemin_dossier: &str) -> Vec<String> {
+    fs::read_dir(chemin_dossier)
+        .expect("Erreur lors de la lecture du dossier")
+        .filter_map(|entry| {
+            entry
+                .ok()
+                .map(|e| e.file_name().to_string_lossy().to_string())
+        })
+        .collect()
 }
